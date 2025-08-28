8be42bee41a880b57f121b8d150b71f3c45158e3
justas <justas.draksas@gmail.com>
2025-08-28 18:07:28 +0300

    Fix clipboard on debian

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.clipboard = "unnamedplus"
-vim.opt.relativenumber = false
+vim.opt.clipboard = "unnamedplus" -- Make the clipboard work
+vim.opt.relativenumber = false -- Number the lines
+vim.opt.wrap = true -- Make sure the screen does not scroll right
